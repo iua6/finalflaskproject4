3d18a05d0bffb69b9476308b446d67734569c634
ike <iua6@njit.edu>
2022-05-11 15:51:55 -0400

    "dashboard modification"

--- app/auth/forms/_init_.py
+++ app/auth/forms/_init_.py
@@ -14,26 +14,35 @@ class login_form(FlaskForm):
     ])
     submit = SubmitField()
 
-
 class register_form(FlaskForm):
     email = EmailField('Email Address', [
         validators.DataRequired(),
-
     ], description="You need to signup with an email")
 
     password = PasswordField('Create Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match'),
-
     ], description="Create a password ")
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
+    submit = SubmitField()
+
+class create_user_form(FlaskForm):
+    email = EmailField('Email Address', [
+        validators.DataRequired(),
+    ], description="You need to signup with an email")
+
+    password = PasswordField('Create Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+    ], description="Create a password ")
+    confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
+    is_admin = BooleanField('Admin', render_kw={'value':'1'})
     submit = SubmitField()
 
 
 class profile_form(FlaskForm):
     about = TextAreaField('About', [validators.length(min=6, max=300)],
                           description="Please add information about yourself")
-
     submit = SubmitField()
 
 class user_edit_form(FlaskForm):
@@ -42,11 +51,9 @@ class user_edit_form(FlaskForm):
     is_admin = BooleanField('Admin', render_kw={'value':'1'})
     submit = SubmitField()
 
-
 class security_form(FlaskForm):
     email = EmailField('Email Address', [
         validators.DataRequired(),
-
     ], description="You can change your email address")
 
     password = PasswordField('Create Password', [
@@ -55,7 +62,6 @@ class security_form(FlaskForm):
 
     ], description="Create a password ")
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
-
     submit = SubmitField()
 
 class csv_upload(FlaskForm):
